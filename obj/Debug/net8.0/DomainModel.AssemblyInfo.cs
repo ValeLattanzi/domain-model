6561aacee7ec58245174893587c8a64fdc8ee713
ValeLattanzi <vale.lattanzi15@gmail.com>
2024-09-08 16:15:16 -0300

    feat: create client

--- obj/Debug/net8.0/DomainModel.AssemblyInfo.cs
+++ obj/Debug/net8.0/DomainModel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa9f4aae8f19ac18ccfdead526eac488f076f7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85dc470dfcecad3c214d0cb74739734252bc7c2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DomainModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
